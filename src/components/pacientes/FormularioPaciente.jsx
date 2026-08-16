be994956f5a1240b8bce6c70a8c6708709665542
Alcidesssz <alcidesssz1310@gmail.com>
2026-08-16 14:06:45 -0300

    added datos Obra Social

--- src/components/pacientes/FormularioPaciente.jsx
+++ src/components/pacientes/FormularioPaciente.jsx
@@ -207,7 +207,38 @@ const FormularioPaciente = () => {
                 />    
             </div>
         </div>
-                    </div>
+        <h4 className={styles.subtitulo}>Obra Social</h4>
+        <div className={styles.formGroup}>
+            <div>
+                <label>Obra Social*</label>
+                <select
+                    className={styles.campoInput}
+                    name="obraSocial.nombre"
+                    value={paciente.obraSocial.nombre}
+                    onChange={handleChange}
+                    >
+                        <option value="">Seleccionar Obra Social</option>
+                        <option value="OSDE">OSDE</option>
+                        <option value="SWISS MEDICAL">SWISS MEDICAL</option>
+                        <option value="GALENO">GALENO</option>
+                        <option value="MEDIFE">MEDIFE</option>
+                        <option value="OSFA">OSFA</option>
+                        <option value="OTRO">OTRO</option>
+                        <option value="NINGUNA">NINGUNA</option>
+                </select>
+                </div> 
+                <div>
+                <label>Numero Afiliado*</label>
+                <input
+                    type="text"
+                    name="obraSocial.numeroAfiliado"
+                    value={paciente.obraSocial.numeroAfiliado}
+                    className={styles.campoInput}
+                    onChange={handleChange}
+                />    
+                </div>
+            </div>   
+            </div>
                 
                 <button type="submit" className={styles.btnGuardar}>
           Guardar Paciente
